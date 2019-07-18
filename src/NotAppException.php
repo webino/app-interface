72d1bc853c1466b90d401f06e8d5a63be204d425
bacinsky <peter@bacinsky.sk>
2019-07-18 02:26:10 +0200

    WIP

---
+++ src/NotAppException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Webino™ (http://webino.sk)
+ *
+ * @link        https://github.com/webino/app-interface
+ * @copyright   Copyright (c) 2019 Webino, s.r.o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     BSD-3-Clause
+ */
+
+namespace Webino;
+
+/**
+ * Class NotAppException
+ * @package app-interface
+ */
+class NotAppException extends RuntimeException implements AppExceptionInterface
+{
+
+}
